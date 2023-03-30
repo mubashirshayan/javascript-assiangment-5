3b5a5d395f86f1112b02a1f6de7e632038a7a81e
mubashirshayan <121729482+mubashirshayan@users.noreply.github.com>
2023-03-30 19:31:48 +0500

    new files upload

---
+++ app.js
@@ -0,0 +1,125 @@
+//Question no1
+/*var arr=new Array('','','','')
+console.log(arr)*/
+
+//Question no2
+/*var arr=['','','','','',]
+console.log(arr)*/
+
+//Question no3
+/*var arr=['','','','','']
+console.log(arr)*/
+//Question no4
+/*var arr=[1,2,3,4,5]
+console.log(arr)*/
+//Question no5
+/*var arr=[true,false,true,false]
+console.log*/
+//Question no6
+/*var mixArr=['string',true,63,'name',12]
+console.log(mixArr)*/
+//Question no7
+//var eduQualf=['SSC', 'HSC', 'BCS', 
+//'BS', 'BCOM', 'MS', 'M. Phil', 'PhD']
+  /*document.write('1)'+eduQualf[0]+'</br>')
+  document.write('2)'+eduQualf[1]+'</br>')
+document.write('3)'+eduQualf[2]+'</br>') 
+  document.write('4)'+eduQualf[3]+'</br>')
+  document.write('5)'+eduQualf[4]+'</br>')
+    document.write('6)'+eduQualf[5]+'</br>')
+ document.write( '7)'+eduQualf[6]+'</br>')  
+ document.write('8)'+eduQualf[7]+'</br>')*/
+/*for  (var i=0; i<eduQualf.length;i++){
+  document.write(eduQualf[i]+'</br>');
+  
+}
+console.log(i)*/
+//Question no8
+/*var arr=['Bilal','Saad','Shayan']
+var score=[390,340,450]
+var total=500
+var per0=(score[0]*100)/500
+var per1=(score[1]*100)/500
+var per2=(score[2]*100)/500
+document.write('Score of Bilal is'+score[0]+'. Persentage:'+per0+'%'+'<br>')
+document.write('Score of Saad is'+score[1]+'. Persentage:'+per1+'%'+'<br>')
+document.write('Score of Shayan is'+score[2]+'. Persentage:'+per2+'%'+'<br>')*/
+//Question no 9
+//var color=['black','yellow','blue']
+//document.write(color+'<br>');
+//case a'enter
+/*alert('what color he/she wants to add to thebeginning & add that color to the beginning of the array.')
+var ask=prompt('enter your favorite color')
+color.unshift(ask);
+document.write(color+'<br>');*/
+//case b
+/*alert('what color he/she wants to add to the end & add that color to the end of the array.')
+var ask=prompt('enter your favorite color')
+color.push(ask);
+document.write(color+'<br>');*/
+//case c
+/*color.unshift('orange','graps')
+document.write(color+'<br>');*/
+//case d
+/*color.shift();
+document.write(color+'<br>');*/
+//case e
+/*color.pop();
+document.write(color+'<br>');*/
+//case f
+/*alert('which index he/she wants to add a color & color name. Then add the color to desired position/index.')
+var ask=+prompt('enter index number to add a color')
+var ask2=prompt('enter color name');
+color.splice(ask,0,ask2);
+document.write(color);*/
+
+//case g
+/*alert('which index he/she wants to deletecolor(s) & how many colors he/she wantsto delete.')
+var ask=+prompt('enter position to delete a color')
+var input=+prompt('enter index number to delete a color')
+var retrn=color.splice(ask,input);
+document.write(color);
+console.log(retrn);*/
+//Question no 10
+//var socOfStd=[245,440,350,435,230,500]
+/*alert('Score of students'+socOfStd)
+socOfStd.sort();
+alert('Ordered score of students'+socOfStd)*/
+//Question no11
+/*var citiesLst=['karachi','lahore','islamabad','multan','peshawar','queeta']
+//citiesLst.splice(2,2)
+var sel=citiesLst.slice(2,1)
+console.log(sel)*/
+
+//Question no12
+/*var arr = ['This',' is','my','cat'];
+alert(arr)
+arr.join(arr);
+alert(arr);*/
+    //Question no 13
+  //By Fifo method 
+/*var arr=['keyboard','mouse','printer','monitor']
+document.write('Devices:'+'<br>'+arr+'<br>')
+var first=arr.shift();
+var sec=arr.shift();
+var thd=arr.shift();
+var forth=arr.shift();
+document.write('Out :'+first+'<br>')
+document.write('Out :'+sec+'<br>')
+document.write('Out :'+thd+'<br>')
+document.write('Out :'+forth+'<br>')*/
+//Question no14
+/*var arr=['keyboard','mouse','printer','monitor']
+document.write('Devices:'+'<br>'+arr+'<br>')
+var first=arr.pop();
+var sec=arr.pop();
+var thd=arr.pop();
+var forth=arr.pop();
+document.write('Out :'+first+'<br>')
+document.write('Out :'+sec+'<br>')
+document.write('Out :'+thd+'<br>')
+document.write('Out :'+forth+'<br>')
+console.log(forth)*/
+//Question no15
+
+
